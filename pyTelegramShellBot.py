37227f2a7e1b3e398a02529ec5eceb6583f73233
pjjacob98 <97329107+pjjacob98@users.noreply.github.com>
2022-05-17 11:58:45 +0530

    Update pyTelegramShellBot.py

--- pyTelegramShellBot.py
+++ pyTelegramShellBot.py
@@ -822,9 +822,11 @@ def savePhoto(doc):
         new_file.write(downloaded_file)
     bot.send_message(doc.chat.id, f"File saved as {file_path}")
 
-    def main():
+
+def main():
     bot.polling(none_stop=True)
 
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     print("Bot running...")
     main()
